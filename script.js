e8d7a335a4dc4c540e48ee686779e1badd4d8f96
Najaf Sharifov <nwerifov@mail.ru>
2023-02-19 21:39:13 +0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,18 @@
+
+// Task 2.1 
+
+let kmhSpeed = 180;
+
+let msSpeed = (kmhSpeed * 1000)/3600;
+
+console.log(msSpeed);
+
+
+
+
+// Task 2.1 Bonus
+
+let userAge = prompt('When were you born');
+let currentYear = 2023; 
+alert((currentYear - userAge) * 365);
+
